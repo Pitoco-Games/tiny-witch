ecbdabf3c15af6494eb7a0ae408e3702e1bf0ab6
SirBernhart <behornerl@gmail.com>
2025-03-17 21:41:41 -0300

    MapChangeController.cs - Implement auto and manual saves

--- Tiny Witch/Assets/Scripts/Core Gameplay/MapNavigation/MapChangeController.cs
+++ Tiny Witch/Assets/Scripts/Core Gameplay/MapNavigation/MapChangeController.cs
@@ -3,29 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Utils;
+using Utils.Save;
 
-public class MapChangeController : MonoBehaviour
+public class MapChangeController : MonoBehaviour, IUpdatesSave
 {
     [SerializeField] private GameObject playerPrefab;
     [SerializeField] private List<MapChangeDetector> mapChangeDetectors;
     [SerializeField] private Transform initialSpawnPoint;
     private static MapChangeData lastMapChangeTriggeredData;
 
+    private PlayerLocationSaveData locationSaveData;
+
     private void Awake()
     {
+        SaveService saveService = ServicesLocator.Get<SaveService>();
+        locationSaveData = (PlayerLocationSaveData) saveService.TryGetSavedData<PlayerLocationSaveData>();
+
+        locationSaveData.AddListenerToSaveUpdateRequest(UpdateSaveData);
+
         foreach (MapChangeDetector mapChangeDetector in mapChangeDetectors)
         {
             mapChangeDetector.Setup(HandlePlayerDetected);
         }
 
         GameObject player = Instantiate(playerPrefab, GetPositionToSpawnIn(), Quaternion.identity);
+
+        saveService.SaveAll();
+    }
+
+    private void OnDestroy()
+    {
+        locationSaveData.RemoveListenerToSaveUpdateRequest(UpdateSaveData);
+    }
+
+    public void UpdateSaveData()
+    {
+        locationSaveData.lastVisitedLocationSceneName = SceneManager.GetActiveScene().name;
     }
 
     private Vector2 GetPositionToSpawnIn()
     {
         if(lastMapChangeTriggeredData == null)
         {
-            return initialSpawnPoint != null ? initialSpawnPoint.position : transform.position;
+            return locationSaveData.HasSavedData ? new Vector2(locationSaveData.locationX, locationSaveData.locationY) : initialSpawnPoint.position;
         }
 
         foreach(MapChangeDetector mapChangeDetector in mapChangeDetectors)
